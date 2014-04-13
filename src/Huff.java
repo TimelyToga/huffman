409d7e539684af51d94d17fc591428e227b367fd
Timothy Blumberg <tblumberg39@gmail.com>
2014-04-12 23:47:54 -0400

    first commit

---
+++ src/Huff.java
@@ -0,0 +1,15 @@
+
+/**
+ * Main/launch program for Huff assignment. A better
+ * comment than this is warranted.
+ * @author Timothy Blumberg
+ *
+ */
+public class Huff {
+
+    public static void main(String[] args){
+        HuffViewer sv = new HuffViewer("Duke Compsci Huffing");
+        IHuffProcessor proc = new SimpleHuffProcessor();
+        sv.setModel(proc);    
+    }
+}
