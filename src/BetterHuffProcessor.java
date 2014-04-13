55baaac2823f51fdcfff805c2c1b8023035f43e5
Timothy Blumberg <tblumberg39@gmail.com>
2014-04-12 23:50:22 -0400

    adding the processor

---
+++ src/BetterHuffProcessor.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class BetterHuffProcessor implements IHuffProcessor {
+
+	@Override
+	public void setViewer(HuffViewer viewer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int preprocessCompress(InputStream in) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int compress(InputStream in, OutputStream out, boolean force) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int uncompress(InputStream in, OutputStream out) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
