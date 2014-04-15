9fd8e1cad7a3d12580b5b2b49a87e41e23762fa4
esonmez <esonmez@me.com>
2014-04-15 00:01:16 -0400

    f

--- src/Huff.java
+++ src/Huff.java
@@ -1,4 +1,4 @@
-
+\
 /**
  * Main/launch program for Huff assignment. A better
  * comment than this is warranted.
@@ -6,7 +6,6 @@
  *
  */
 public class Huff {
-
     public static void main(String[] args){
         HuffViewer sv = new HuffViewer("Duke Compsci Huffing");
         IHuffProcessor proc = new SimpleHuffProcessor();
